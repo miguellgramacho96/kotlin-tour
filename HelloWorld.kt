9e38a032c94ff1f891d6fbd9016d372346ef3568
Miguel Gramacho (CTW) <Miguel.Gramacho@ctw.bmwgroup.com>
2023-07-03 19:31:42 +0100

    Add first section of kotlin tour = hello world. Add gitignore and ignore idea folder.

---
+++ HelloWorld.kt
@@ -0,0 +1,19 @@
+val popcorn = 5
+val hotdog = 7
+var customers = 10
+
+fun main() {
+    println("Hello world")
+
+    customers = 8
+    println("There are $customers customers")
+    println("There are ${customers + 1} customers")
+
+    exercise()
+}
+
+fun exercise() {
+    val name = "Mary"
+    val age = 20
+    println("$name is $age years old")
+}
